1606ff48aa493a2d808b49ff3f4d3216290260f7
ivddorrka <ivddorrka@gmail.com>
2021-01-02 06:07:34 +0200

    Needs to be fixed

--- workingequat.py
+++ workingequat.py
@@ -67,6 +67,7 @@ def step_1(eq):
     root_here = [] # here are roots
     for i in ans[1]:
         root_here.append(i[0])
+
     vec = [_[1] for _ in known_r]
     for i in range(len(known_r)):
         ans = [j**i for j in root_here]
@@ -75,22 +76,52 @@ def step_1(eq):
 # print(step_1('r = 7*r -10*r'))
 
 def step_2(eq):
+    """
+    For equations, which have some similar roots
+    C1 + C3 = 2
+    -2*C1 -2*C2 -3*C3 = 9
+    4*C1 + 8*C2 + 9*C3 = 29
+    >>> 'r = -7*r -16*r -12*r'
+    ([[1, 2, 1], [-2, -4, -3], [4, 8, 9]], [2, 9, 29])
+    """
     known_r = [[0, 2], [1, 9], [2, 29]]
-    # ans = check_one(korni(eq))
-    ans = [('Appear more than 1 time: (root, num of rep-s)', [(-2, 2), (3, 3)]), ('others:', [(-3, 1)])]
+    ans = check_one(korni(eq))
+    # ans = [('Appear more than 1 time: (root, num of rep-s)', [(-2, 2)]), ('others:', [(-3, 1), (2, 1)])]
 
     root_here = [] # all roots (as many times as they appear)
-    
+    vec = [el[1] for el in known_r]
+    ind_r = [ele[0] for ele in known_r]
     for i in range(len(ans[0][1])):
         promm = []
         for _ in range((ans[0][1][i][1])):
             promm.append(ans[0][1][i][0])
         root_here.append(promm)
 
-    for j in range(len(ans[1][1][0])):
-        for elem in range(ans[1][1][0][j]):
-            # root_here.append(ans[1][1][0][0])
-            root_here.append(ans[1][1][0][0])
-
-    return root_here
+    for j in range(len(ans[1][1])):
+        for elem in range(ans[1][1][j][1]):
+            root_here.append([ans[1][1][j][0]])
+    emmp = []
+    for step in ind_r:
+        sp = []
+        for spisok in root_here:
+            new_1 = []
+            for elements in spisok:
+                elements = elements**step
+                new_1.append(elements)
+            sp.append(new_1)
+        emmp.append(sp)
+    
+    lst = []
+    for number in range(len(ind_r)):
+        # new_2 = []
+        for spis in emmp:
+            new_2 = []
+            # for number in ind_r:
+            for x in spis:
+                # new_2 = []
+                for y in range(len(x)):
+                    res = x[y]*(ind_r[number]**y)
+                    new_2.append(res)
+            lst.append(new_2)
+    return lst
 print(step_2('r = -7*r -16*r -12*r'))
