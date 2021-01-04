e5a35629a6ad080903946aea7a143377ab6622ad
ivddorrka <ivddorrka@gmail.com>
2021-01-04 18:15:12 +0200

    Modified tests

--- readingfiles.py
+++ readingfiles.py
@@ -6,7 +6,7 @@ import time
 
 def intruction(file):
     """
-    to read "intro" file
+    to read all text file 
     """
     readingg = open(file, "r")
     return '\n'.join(map(str, list(readingg)))
@@ -16,8 +16,11 @@ def userequat():
     """
     to get the start equation
     """
-    type_here = input("Введіть ваше рівняння згідно описаних правил: ")
+    type_here = input("Введіть ваше рівняння згідно описаних правил ЛАТИНИЦЕЮ: ")
     if 'r' not in type_here.split() or '=' not in type_here.split():
+        print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
+        return userequat()
+    if len(type_here.split()) < 3:
         print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
         return userequat()
     else:
@@ -30,37 +33,26 @@ def known_roots():
     """
     list_korn = []
     print(2*'\n')
-    print("Введіть всі відомі або задані корені так, як показано в прикладі: індекс кореня, його значення " +'\n')
+    print("Введіть всі відомі або задані корені послідовно: " +'\n')
     time.sleep(1)
     print("Приклад:" + '\n')
     time.sleep(1)
-    print("0 2")
+    print("2" + '\n' + '1')
     time.sleep(1)
     while(True):
-        # list_korn = []
         user_input = input("Вводіть тут: ")
-        list_korn.append([int(i) for i in user_input.split()])
+        try:
+            list_korn.append(user_input)
+        except ValueError:
+            print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
+            time.sleep(1)
+            return known_roots()
         if user_input != '':
             continue
         if user_input == '':
             del list_korn[-1]
             break
 
-    # для безкінечного введення відомих коренів
-
-    for i in list_korn:
-        if len(i) != 2:
-            time.sleep(0.5)
-            print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
-            return known_roots()
-
-            #цикл тут перевіряє на наявність і правильність введених данних
-    
-    return list_korn
-
-# функція повертає список списків, в кожному з яких:
-# перший елемент - номер кореня, а другий - значення цього кореня
+    return [int(j) for j in list_korn]
 
 # print(known_roots())
-
-# ПО СУТІ ЦЕ СТАРТ ПРОЕКТУ
