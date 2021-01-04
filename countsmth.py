606641396bb37f8b1a7fb7b83d8df89293d1b46e
ivddorrka <ivddorrka@gmail.com>
2021-01-04 21:47:18 +0200

    Finished count function

--- countsmth.py
+++ countsmth.py
@@ -8,30 +8,26 @@ from sympy import roots
 
 def matritsa(tup):
     """
+    >>>
+
     """
     matr = np.array(tup[0])
     vector = np.array(tup[1])
     result = np.linalg.solve(matr, vector)
     return result
-
-print(matritsa(([[2, 5], [1, -10]], [1, 3])))
-
-# ([[1, 1], [1, -1]], [8, 9]]))
-
-# x + y = 8
-# 2x - y = 9 
-
+print(matritsa([[[0, 1], [1, 1]], [4, 1]]))
 
 def rivnynnya(equt):
     """
-    Function to find al roots (returns a list of them)
+    Function to find all roots (returns a dict of them)
     Here: key - root; value - number of its repetitions 
     >>> 'r**2 - 2* r + 1'
     {1: 2}    
     """
     result = roots(equt)
     return result
-# print(rivnynnya('r**2 - r'))
+# print(rivnynnya('r**3 - r**2 - -4*r**1 - +3*r**0'))
+
 
 def  cool_out(mn):
     """
