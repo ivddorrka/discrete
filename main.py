4954d946835d02201ecd81ed260ca3b0cc9ff7aa
ivddorrka <ivddorrka@gmail.com>
2021-01-04 18:14:22 +0200

    Finished main module

--- main.py
+++ main.py
@@ -3,68 +3,109 @@ Main File, with all functions and work done
 """
 import time
 
-from countsmth import rivnynnya, matritsa
+from countsmth import rivnynnya
 from readingfiles import intruction, userequat, known_roots
-from workingequat import korni, check_one, step_2, find_c, func1, func2
+from rivn import calc_matrices, beat_output, all_elements
+
 
 print(intruction("intro"))
 time.sleep(2)
 
 print(2*'\n')
 
-print(intruction("choice_first"))
-print('\n')
+# print(intruction("choice_first"))
+# print('\n')
 
 def asking_output():
-    answer = input("Type here: ")
-    print('\n')
-    if answer not in ['1', '2', '3']:
-        time.sleep(1)
-        print("Try that again")
-        time.sleep(1)
+    
+    def mainn():
+        print(intruction("choice_first"))
         print('\n')
-        return asking_output()
-    if answer == '1':
         time.sleep(1)
-        print("Приклад: " + '\n' + 'r**1 - r**0' + '\n')
-        time.sleep(1)
-        ask_2 = input("Введіть рівняння формату як в прикладі: ")
-        print("Це <'r**1 - r**0'> те сме, що і <'r**1 - r**0' = 0>")
-        print("Не додавайте '=0' в кінці!")
-        try: 
-            return rivnynnya(ask_2)
-        except SyntaxError:
+        answer = input("Type here: ")
+        print('\n')
+        if answer not in ['1', '2', '3', '4']:
             time.sleep(1)
-            print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
-            return asking_output()
-    if answer == '2':
-        print(intruction("intro"))
-        time.sleep(2)
-        rivn = userequat()
-        known_r = known_roots()
-        time.sleep(1)
-        ask_3 = input("Введіть кількість перших членів: ")
-        time.sleep(1)
-        # rivn = userequat()
-        try:
-            return func2(rivn, int(ask_3), known_r)
-        except ValueError:
+            print("Try that again")
             time.sleep(1)
-            print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
-            return asking_output()
-    if answer == '3':
-        print(intruction("intro"))
-        time.sleep(2)
-        rivn = userequat()
-        time.sleep(1)
-        known_r = known_roots()
-        time.sleep(1)
-        ask_3 = input("Введіть кількість перших членів: ")
-        time.sleep(1)
-        try:
-            return func1(rivn, int(ask_3), known_r)
-        except ValueError:
+            print('\n')
+            print(asking_output())
+            mainn()
+        if answer == '1':
             time.sleep(1)
-            print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
-            return asking_output()
-print(asking_output())
+            print("Приклад: " + '\n' + 'r**1 - r**0' + '\n')
+            time.sleep(1)
+            ask_2 = input("Введіть рівняння формату як в прикладі: ")
+            print("Це <'r**1 - r**0'> те сме, що і <'r**1 - r**0' = 0>")
+            print("Не додавайте '=0' в кінці!")
+            # mainn()
+            try: 
+                print(rivnynnya(ask_2))
+            except SyntaxError:
+                time.sleep(1)
+                print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
+                print(asking_output())
+            mainn()
+        if answer == '2':
+            print(intruction("instructions"))
+            time.sleep(2)
+            rivn = userequat()
+            known_r = known_roots()
+            time.sleep(1)
+            ask_3 = input("Введіть кількість перших членів: ")
+            time.sleep(1)
+            # rivn = userequat()
+            try:
+                print(beat_output(all_elements(rivn, known_r, int(ask_3))))
+            except ValueError:
+                time.sleep(1)
+                print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
+                print(asking_output())
+            mainn()
+        if answer == '3':
+            print(intruction("instructions"))
+            time.sleep(2)
+            rivn = userequat()
+            time.sleep(1)
+            known_r = known_roots()
+            time.sleep(1)
+            ask_3 = input("Введіть номер шуканого члена: ")
+            time.sleep(1)
+            try:
+                print('a({}) = {}'.format(int(ask_3), calc_matrices(rivn, known_r, int(ask_3))))
+            except ValueError:
+                time.sleep(1)
+                print("ВиНиКлА пОмИлКа 0_о" + '\n' + 'Спробуйте ще раз!')
+                print(asking_output())
+            mainn()
+        if answer == '4':
+            print('Дякую, що скористалися, до зустрічі')
+
+    mainn()
+asking_output()
+# time.sleep(1)
+
+# def cicl():
+#     """
+#     to ask many times
+#     """
+#     def mainnn():
+
+#         print("Чи бажаєте використати програму ще раз?")
+#         time.sleep(1)
+#         print("Так - введіть 1" + '\n' + 'Ні - введіть 2')
+#         time.sleep(1)
+#         answer = input("Вводіть тут: ")
+#         if answer == '2':
+#             print('Дякую, що скористалися, до зустрічі')
+
+#         if answer == '1':
+#             print(asking_output())
+#             time.sleep(1)
+#             mainnn()
+#         else: 
+#             print("ERROR")
+#             time.sleep(1)
+#             mainnn()
+#     mainnn()
+# cicl()
