9ebae359da3a2397c874e3e31494cc139f7ce503
ivddorrka <ivddorrka@gmail.com>
2021-01-04 20:27:03 +0200

    Adding main

--- workingequat.py
+++ workingequat.py
@@ -55,46 +55,45 @@ def step_2(eq, known_r):
     # known_r = known_roots()
 
     root_here = [] # all roots (as many times as they appear)
-    # vec = [el[1] for el in known_r]
-    # ind_r = [ele[0] for ele in known_r]
-#     num_roots = check_one(korni(eq))
-#     for i in num_roots:
-#         prom = []
-#         for j in range((i[1])):
-#             prom.append(i[0])
-#         root_here.append(prom)
+    vec = [el[1] for el in known_r]
+    ind_r = [ele[0] for ele in known_r]
+    num_roots = check_one(korni(eq))
+    for i in num_roots:
+        prom = []
+        for j in range((i[1])):
+            prom.append(i[0])
+        root_here.append(prom)
 
-#     emmp = [] # кількість списків = кількості данних коренів, взято в степінь
-#     for step in ind_r:
-#         sp = []
-#         for spisok in root_here:
-#             new_1 = []
-#             for elements in spisok:
-#                 elements = elements**step
-#                 new_1.append(elements)
-#             sp.append(new_1)
-#         emmp.append(sp)
+    emmp = [] # кількість списків = кількості данних коренів, взято в степінь
+    for step in ind_r:
+        sp = []
+        for spisok in root_here:
+            new_1 = []
+            for elements in spisok:
+                elements = elements**step
+                new_1.append(elements)
+            sp.append(new_1)
+        emmp.append(sp)
     
-#     tabl = []
+    tabl = []
 
-#     for _ in ind_r:
-#         new_y = []
-#         for el in emmp:
-#             new_x = []
-#             for spiski in el:
-#                 # answer = []
-#                 for elem in range(len(spiski)):
-#                     res = int(spiski[elem])*(int(ind_r[_])**int(elem))
-#                     new_x.append(res)
-#                 # new_x.append(answer)
-#             new_y.append(new_x)
-#         tabl.append(new_y)
-#     lst = [] # the list of lists of needed coef-s acco-g to rows
-#     for pix in range(len(tabl)):
-#         lst.append(tabl[pix][pix])
-    # return [lst, vec]
-    return known_r
-print(step_2('r = -7*r -16*r -12*r', [[0, 2], [1, 9], [2, 29]]))
+    for _ in ind_r:
+        new_y = []
+        for el in emmp:
+            new_x = []
+            for spiski in el:
+                # answer = []
+                for elem in range(len(spiski)):
+                    res = int(spiski[elem])*(int(ind_r[_])**int(elem))
+                    new_x.append(res)
+                # new_x.append(answer)
+            new_y.append(new_x)
+        tabl.append(new_y)
+    lst = [] # the list of lists of needed coef-s acco-g to rows
+    for pix in range(len(tabl)):
+        lst.append(tabl[pix][pix])
+    return [lst, vec]
+# print(step_2('r = -7*r -16*r -12*r', [[0, 2], [1, 9], [2, 29]]))
 
 
 def find_c(eq, known_r):
@@ -136,7 +135,7 @@ def func1(eq, n, known_r):
             result.append((n**ind_el) * element)
     return round(float(sum(result)), 2)
 # print(func1('r = -3*r +13*r +15*r', [[0, ]]3))
-print(func1('r = -7*r -16*r -12*r', [[0, 2], [1, 9], [2, 29]], 3))
+print(func1('r = -7*r -16*r -12*r', 1000, [[0, 2], [1, 9], [2, 29]]))
 
 def func2(eq, n, known_r):
     result = []
